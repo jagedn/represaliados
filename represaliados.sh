717a14ccc2bb247c14c55ff591e1892dbf0a8c60
Jorge <jagedn@gmail.com>
2019-11-30 10:22:09 +0100

    Update represaliados.sh

--- represaliados.sh
+++ represaliados.sh
@@ -1,9 +1,2 @@
 curl https://raw.githubusercontent.com/jagedn/represaliados/master/represaliados.groovy > represaliados.groovy
-i=$1
-while true 
-do
-echo $i
-groovy represaliados.groovy $i
-i=$((i+1))
-sleep $(( 80 + (RANDOM % 10) + (RANDOM % 10) + 1 ))
-done
+groovy represaliados.groovy $1
